b5674625195a39577de8854ec231bcdc9f78b541
kaiweifan11 <30461398+kaiweifan11@users.noreply.github.com>
2024-12-16 14:36:23 +0800

    Add files via upload

---
+++ Lyrical-GraphQL/client/index.js
@@ -0,0 +1,37 @@
+import './style/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
+const client = new ApolloClient({
+  // link the data in the response and issues an id
+  // so that apollo will understand that when this object is mutated
+  // it will automatically ask react to rerender 
+  //https://www.apollographql.com/docs/react/caching/cache-configuration
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App} >
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  )
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
